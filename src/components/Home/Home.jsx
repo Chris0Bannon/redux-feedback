f30f79369ea52a3f33f9f456c17b872799328ce1
Christopher O'Bannon <Crobwan@gmail.com>
2019-08-24 15:49:29 -0500

    creates a next button in Home Component that advances to next Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,22 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Home extends Component {
+
+nextHandler = (event) => {
+    console.log('you clicked the next button');
+    
+    this.props.history.push('feeling');
+}
+
+
   render() {
-    return <h1>Hello From Home</h1>;
+    return (
+      <div>
+        <h1>Hello From Home</h1>
+        <p>Welcome to your daily feedback.  Click the NEXT button to proceed</p>
+        <button onClick = {this.nextHandler} >NEXT</button>
+      </div>
+    );
   }
 }
 
